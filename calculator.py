50aaa6b4876c038be99fe270cd760f507241033f
iinuiinu <tanapatra5757@gmail.com>
2024-07-26 10:47:39 +0700

    commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+
+print(add(5,3))
+print(subtract(5,3))
+print(multiply(5,3))
+print(divide(5,3))
