98e2d5c67a4ae03c05fe8cac5395875821c905a1
iinuiinu <tanapatra5757@gmail.com>
2024-07-26 10:37:28 +0700

    commit

---
+++ lambda_calculator.py
@@ -0,0 +1,9 @@
+addition = lambda x, y: x+y
+subtraction = lambda x, y: x-y
+multiplication  = lambda x, y: x*y
+division = lambda x, y: x/y
+
+print(addition(5,4))
+print(subtraction(5,3))
+print(multiplication(5,3))
+print(division(5,3))
---
+++ utilities/string_operations.py
@@ -0,0 +1,8 @@
+def reverse_string(sample_string):
+    return sample_string[::-1]
+def capitalize_string(sample_string):
+    return sample_string.capitalize()
+def lowercase_string(sample_string):
+    return sample_string.lower()
+def uppercase_string(sample_string):
+    return sample_string.upper()
